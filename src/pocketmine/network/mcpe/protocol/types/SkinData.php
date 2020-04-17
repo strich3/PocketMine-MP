2ad06c2fb005eb358f40ff3417407ff6721edb72
strich3 <56207893+strich3@users.noreply.github.com>
2020-04-17 13:30:03 +0200

    Revert "1.14.60 support (#3407)"
    This reverts commit a107ad74048145c1969b2541eb8f053d70337b09.

--- src/pocketmine/network/mcpe/protocol/types/SkinData.php
+++ src/pocketmine/network/mcpe/protocol/types/SkinData.php
@@ -27,9 +27,6 @@ use pocketmine\utils\UUID;
 
 class SkinData{
 
-	public const ARM_SIZE_SLIM = "slim";
-	public const ARM_SIZE_WIDE = "wide";
-
 	/** @var string */
 	private $skinId;
 	/** @var string */
@@ -54,23 +51,11 @@ class SkinData{
 	private $capeId;
 	/** @var string */
 	private $fullSkinId;
-	/** @var string */
-	private $armSize;
-	/** @var string */
-	private $skinColor;
-	/** @var PersonaSkinPiece[] */
-	private $personaPieces;
-	/** @var PersonaPieceTintColor[] */
-	private $pieceTintColors;
-	/** @var bool */
-	private $isVerified;
 
 	/**
-	 * @param SkinAnimation[]         $animations
-	 * @param PersonaSkinPiece[]      $personaPieces
-	 * @param PersonaPieceTintColor[] $pieceTintColors
+	 * @param SkinAnimation[] $animations
 	 */
-	public function __construct(string $skinId, string $resourcePatch, SkinImage $skinImage, array $animations = [], SkinImage $capeImage = null, string $geometryData = "", string $animationData = "", bool $premium = false, bool $persona = false, bool $personaCapeOnClassic = false, string $capeId = "", ?string $fullSkinId = null, string $armSize = self::ARM_SIZE_WIDE, string $skinColor = "", array $personaPieces = [], array $pieceTintColors = [], bool $isVerified = false){
+	public function __construct(string $skinId, string $resourcePatch, SkinImage $skinImage, array $animations = [], SkinImage $capeImage = null, string $geometryData = "", string $animationData = "", bool $premium = false, bool $persona = false, bool $personaCapeOnClassic = false, string $capeId = "", ?string $fullSkinId = null){
 		$this->skinId = $skinId;
 		$this->resourcePatch = $resourcePatch;
 		$this->skinImage = $skinImage;
@@ -84,11 +69,6 @@ class SkinData{
 		$this->capeId = $capeId;
 		//this has to be unique or the client will do stupid things
 		$this->fullSkinId = $fullSkinId ?? UUID::fromRandom()->toString();
-		$this->armSize = $armSize;
-		$this->skinColor = $skinColor;
-		$this->personaPieces = $personaPieces;
-		$this->pieceTintColors = $pieceTintColors;
-		$this->isVerified = $isVerified;
 	}
 
 	public function getSkinId() : string{
@@ -140,38 +120,5 @@ class SkinData{
 
 	public function getFullSkinId() : string{
 		return $this->fullSkinId;
-	}
-
-	public function getArmSize() : string{
-		return $this->armSize;
-	}
-
-	public function getSkinColor() : string{
-		return $this->skinColor;
-	}
-
-	/**
-	 * @return PersonaSkinPiece[]
-	 */
-	public function getPersonaPieces() : array{
-		return $this->personaPieces;
-	}
-
-	/**
-	 * @return PersonaPieceTintColor[]
-	 */
-	public function getPieceTintColors() : array{
-		return $this->pieceTintColors;
-	}
-
-	public function isVerified() : bool{
-		return $this->isVerified;
-	}
-
-	/**
-	 * @internal
-	 */
-	public function setVerified(bool $verified) : void{
-		$this->isVerified = $verified;
 	}
 }
